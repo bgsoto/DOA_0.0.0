63d7c0c4916ccb400ee4bbacd9db43110e60f683
bgsoto <115710071+bgsoto@users.noreply.github.com>
2023-10-14 14:57:32 -0400

    Ai in smaple scene

--- Assets/Animations/animationStateController.cs
+++ Assets/Animations/animationStateController.cs
@@ -7,31 +7,41 @@ public class animationStateController : MonoBehaviour
     [Header("Relationships")]
     [SerializeField] private Animator animator;
     [SerializeField] private MonsterStateMachine Anamoly;
-    [SerializeField] private 
-    
+    [SerializeField] private
 
 
 
-    
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
-       
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Anamoly.currentState == MonsterStateMachine.AnomalyState.Patrol)
+        if (Anamoly.currentState == MonsterStateMachine.AnomalyState.Patrol|| Anamoly.currentState == MonsterStateMachine.AnomalyState.Stalk)
         {
             animator.SetBool("isWalking", true);
         }
         else
         {
             animator.SetBool("isWalking", false);
+        }
+
+        if (Anamoly.currentState == MonsterStateMachine.AnomalyState.Chase)
+        {
+            animator.SetBool("isRunning", true);
+
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
         }
 
 
