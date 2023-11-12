ccdbc271e916a83035ac84c0c5d0d409e5c437db
Somnarng <99498786+Somnarng@users.noreply.github.com>
2023-11-11 23:58:10 -0500

    Merged Objective Updates and Bug Fixes
    -Merged Objective Updates from other branch
    -Adjusted several values for post process on monster
    -Fixed some minor UI and interaction bugs

--- Assets/Scripts/UI/Note/NoteDisplayConditional.cs
+++ Assets/Scripts/UI/Note/NoteDisplayConditional.cs
@@ -10,14 +10,21 @@ public class NoteDisplayConditional : MonoBehaviour, IInteractable
     [SerializeField] private int objectiveToCheck; //objective stage to check against
     [SerializeField] private int altObjectiveToSet; //objective stage to set if current objective is less than check
     [SerializeField] private bool objective2; //if is objective 2, true
+    [SerializeField] private string actionText;
     private int currentObjectiveStage;
     private int objectiveStage;
     private bool noteCollected = false;
+    private ObjectiveManager objectiveManager;
+
+    /* Not used */
+    private ItemData itemData;
+    private bool pickable;
 
     private void Start()
     {
         noteText.text = noteToDisplay.noteText;
         noteTitle.text = noteToDisplay.noteTitle;
+        objectiveManager = GameObject.FindGameObjectWithTag("ObjectiveManager").GetComponent<ObjectiveManager>();
     }
     private void OnEnable()
     {
@@ -35,8 +42,8 @@ public class NoteDisplayConditional : MonoBehaviour, IInteractable
             if (currentObjectiveStage >= objectiveToCheck) { objectiveStage = altObjectiveToSet; }
             else { objectiveStage = noteToDisplay.objectiveStage; }
             noteCollected = true;
-            if (noteToDisplay.isObjective2) { ObjectiveUpdater.UpdateObjectives?.Invoke(true, objectiveStage); }
-            else { ObjectiveUpdater.UpdateObjectives?.Invoke(false, objectiveStage); }//sets either objective 1 or 2 depending on what note effects
+            if (noteToDisplay.isObjective2) { objectiveManager.UpdateObjective(true, objectiveStage); }
+            else { objectiveManager.UpdateObjective(false, objectiveStage); }//sets either objective 1 or 2 depending on what note effects
             NoteDisplay.NoteGathered?.Invoke();
             Destroy(this); //removes interactable.
         }
@@ -49,4 +56,9 @@ public class NoteDisplayConditional : MonoBehaviour, IInteractable
             currentObjectiveStage = questStage;
         }
     }
+
+    public void Use() { return; }
+    public ItemData ItemData { get { return itemData; } set { itemData = value; } }
+    public bool Pickable { get { return pickable; } set { pickable = value; } }
+    public string ActionText { get { return actionText; } set { actionText = value; } }
 }
