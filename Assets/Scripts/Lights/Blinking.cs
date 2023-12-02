14af1066099ed9624f9cee9bd272a24b17867457
Aushuna <Aushunavr@gmail.com>
2023-12-02 14:19:09 -0500

    Blinking Lights

--- Assets/Scripts/Lights/Blinking.cs
+++ Assets/Scripts/Lights/Blinking.cs
@@ -5,37 +5,54 @@ using UnityEngine;
 public class Blinking : MonoBehaviour
 {
 
-    public Light lightOB;
+    public Light lightOB, lightOB1;
 
     public AudioSource lightSound;
 
     public float minTime;
     public float maxTime;
     public float timer;
+    
+    public Material greyMat;
+    public Material whiteMat;
+
+    public MeshRenderer meshRenderer;
 
 
     // Start is called before the first frame update
     void Start()
     {
         timer = Random.Range(minTime, maxTime);
+        meshRenderer.material = whiteMat;
     }
 
     // Update is called once per frame
     void Update()
     {
-        LightsFlickering();   
+        LightsFlickering(); 
+ 
     }
 
     void LightsFlickering()
     {
         if (timer > 0)
             timer -= Time.deltaTime;
+            
+        if (timer <= 10f)
+        {
+            lightOB.enabled = lightOB.enabled;
+            lightOB1.enabled = lightOB1.enabled;
+            meshRenderer.material = whiteMat;
+            timer = Random.Range(minTime, maxTime);
 
-        if (timer <= 0)
+        }
+        if (timer >= 5f)
         {
             lightOB.enabled = !lightOB.enabled;
-            timer = Random.Range(minTime, maxTime);
-            lightSound.Play();
+            lightOB1.enabled = !lightOB1.enabled;
+            meshRenderer.material = greyMat;
+            
+            //lightSound.Play();
         }
     }
 
