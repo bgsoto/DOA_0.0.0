214a2776875717c480eab1a3e7863dba097da65a
bgsoto <115710071+bgsoto@users.noreply.github.com>
2024-03-18 20:58:08 -0400

    Artifact bug fix
    artifact is no collectible and shield is spawned in the capture space of monster

--- Assets/Scripts/Item/Artifact.cs
+++ Assets/Scripts/Item/Artifact.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 
 public class Artifact : MonoBehaviour, IInteractable
 {
+    
     [Header("Settings")]
-    [SerializeField] private bool pickable;
+   // [SerializeField] private bool pickable;
     [SerializeField] private string actionText;
 
     public static Action onArtifactCollected;
 
-    /* Not used */
-    private ItemData itemData;
+    // Not used 
+    private ItemData artifact;
+    private bool pickable = false;
 
     public void Interact()
     {
-        /* Subscription: PlayerInteraction */
+        // Subscription: PlayerInteraction 
         onArtifactCollected?.Invoke();
         Destroy(gameObject);
     }
 
     public bool Pickable { get { return pickable; } set { pickable = value; } }
     public void Use() { return; }
-    public ItemData ItemData { get { return itemData; } set { itemData = value; } }
+    public ItemData ItemData { get { return artifact; } set { artifact = value; } }
     public string ActionText { get { return actionText; } set { actionText = value; } }
+    
 
+    
 }
